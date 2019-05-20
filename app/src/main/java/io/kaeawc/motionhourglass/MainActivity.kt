d28d1a826a477fe7df85c174cc29607ed77de959
Jason Pearson <jason.d.pearson@gmail.com>
2019-05-20 11:31:15 -0400

    initial commit

---
+++ app/src/main/java/io/kaeawc/motionhourglass/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.kaeawc.motionhourglass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
