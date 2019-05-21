2222c2d6531b8e4c1fa29d3ef9935866d5b46ee2
Jason Pearson <jason.d.pearson@gmail.com>
2019-05-21 00:01:04 -0400

    remove unnecessary elements

--- app/src/main/java/io/kaeawc/motionhourglass/MainActivity.kt
+++ app/src/main/java/io/kaeawc/motionhourglass/MainActivity.kt
@@ -2,22 +2,11 @@ package io.kaeawc.motionhourglass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        start_button?.setOnClickListener {
-            motion_scene?.setTransition(R.id.full_1, R.id.empty_1)
-            motion_scene?.progress = 0f
-            motion_scene?.transitionToEnd()
-        }
     }
 }
